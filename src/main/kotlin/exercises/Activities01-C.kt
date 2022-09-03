03da7b57455a128bf44a2de0d4aa082a946f338c
girly <girly.dasalla@gmail.com>
2022-09-03 13:20:10 +0800

    initial commit for Activities01-C.kt

---
+++ src/main/kotlin/exercises/Activities01-C.kt
@@ -0,0 +1,28 @@
+package exercises
+
+fun main(){
+    println("List of Items from Grocery Store")
+
+    //list of grocery wet
+    var string1 : String = "W Tender Juicy Hotdog"
+    var string2 : String = "W Magnolia Chicken Whole"
+    var string3 : String = "W Magnolia Icecream Cookies and Cream"
+
+    //list of grocery dry
+    var string4 : String = "D Gardenia Wheat Bread"
+    var string5 : String = "D Huggies Diaper Pants XL"
+    var string6 : String = "D Century Tuna Flakes in Oil"
+
+    //string character
+    println("Wet Goods")
+    println(string1[0])
+    println("$string1")
+    println("$string2")
+    println("$string3")
+
+    println("Dry Goods")
+    println(string4[0])
+    println("$string4")
+    println("$string5")
+    println("$string6")
+}
