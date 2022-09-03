b582cf0dc9df3adc22d7914c5f6afaa541038895
girly <girly.dasalla@gmail.com>
2022-09-03 12:43:34 +0800

    initial commit for Activities01-B.kt

---
+++ src/main/kotlin/exercises/Activities01-B.kt
@@ -0,0 +1,26 @@
+package exercises
+
+fun main(){
+    println("List of Books")
+
+    //list of books for initial A
+    var string1 : String = "Algebra"
+    var string2 : String = "Acting Skills"
+    var string3 : String = "Alice in Wonderland"
+
+    //list of books for initial B
+    var string4 : String = "Biology"
+
+    //string character
+    println(string1[0])
+    println("$string1")
+    println("$string2")
+    println("$string3")
+
+    println(string4[0])
+    println("$string4")
+
+
+
+
+}
