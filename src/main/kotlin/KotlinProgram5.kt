b974b83c7ba2b364d25e7d4bf63224248c23484b
girly <girly.dasalla@gmail.com>
2022-09-03 16:30:25 +0800

    initial commit for KotlinProgram5.kt

---
+++ src/main/kotlin/KotlinProgram5.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var itemNumber:Int = 2
+
+    when(itemNumber){
+        1 -> print("clothes")
+        2 -> print("food")
+        3 -> print("toiletries")
+        else -> print("invalid item number")
+    }
+}
