d70ce5fd3ba37cb1b67266397b7e4a6317e748ca
girly <girly.dasalla@gmail.com>
2022-09-03 16:31:50 +0800

    initial commit for KotlinProgram3.kt

---
+++ src/main/kotlin/KotlinProgram3.kt
@@ -0,0 +1,22 @@
+fun main(){
+    var number1 : Int = 21
+    var number2 : Int = 21
+
+    var answer : Int = number1 % number2
+
+    println("Answer is $answer")
+
+    //complex operations
+    var complexFormula: Int =(2*10)+(10/2)
+    println(complexFormula)
+
+    //comparison
+    var expression :  Boolean = !(number1 < number2)
+    println(expression)
+
+    //increment operator
+    var incrementNum : Int = ++number1
+    println(incrementNum)
+
+
+}
