5ca5fe149d24b916deae121510ac70fd662f8628
girly <girly.dasalla@gmail.com>
2022-09-03 11:39:36 +0800

    initial commit for Activities01-A.kt

---
+++ src/main/kotlin/exercises/Activities01-A.kt
@@ -0,0 +1,38 @@
+package exercises
+
+fun main(){
+    println("Hello from Gelay")
+
+    //students data
+    //student 1
+    println("Enter students name1:")
+    var name1 : String = readln().toString()
+
+    println("Enter Course:")
+    var course1 : String = readln().toString()
+
+    println("Enter student grade")
+    var grade1 : Int = readln().toInt()
+
+    //student 2
+    println("Enter students name2:")
+    var name2 : String = readln().toString()
+
+    println("Enter Course:")
+    var course2 : String = readln().toString()
+
+    println("Enter student grade")
+    var grade2 : Int = readln().toInt()
+
+    //student 3
+    println("Enter students name3:")
+    var name3 : String = readln().toString()
+
+    println("Enter Course:")
+    var course3 : String = readln().toString()
+
+    println("Enter student grade")
+    var grade3 : Int = readln().toInt()
+
+
+}
