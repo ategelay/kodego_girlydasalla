f34ee76797b31c35445adc2c97cbf312c8a2ef91
girly <girly.dasalla@gmail.com>
2022-09-05 19:40:18 +0800

    initial commit for Program8.kt

---
+++ src/main/kotlin/KotlinProgram8.kt
@@ -0,0 +1,24 @@
+fun main(){
+//    var ctr : Int = 1
+//
+//    ctr++
+//    println(ctr)
+//}
+//
+//    var ctr : Int = 1
+//
+//    ctr--
+//    println(ctr)
+//}
+
+    //while loop
+    //init
+    var ctr : Int = 1
+    //condition
+    while(ctr <=5 ) {
+        println("Hello World! $ctr")
+        //update
+        ctr++
+    }
+}
+
