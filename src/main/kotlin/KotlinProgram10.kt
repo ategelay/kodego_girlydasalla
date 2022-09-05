d7d7022dd058932beb23853a2a7b581407d4f5bd
girly <girly.dasalla@gmail.com>
2022-09-05 20:18:34 +0800

    initial commit for Program10.kt

---
+++ src/main/kotlin/KotlinProgram10.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var ctr1 : Int = 1
+    while (ctr1 <= 5){          //@@@@@
+        var ctr2 : Int = 1
+        while(ctr2 <=5){
+                print("@")  //output @@@@@
+                ctr2++
+        }
+        println()
+        ctr1++
+    }
+}
