3cb83e00063b0a8f35c800b530ecfa168705df9a
girly <girly.dasalla@gmail.com>
2022-09-06 19:36:30 +0800

    initial commit for KotlinProgram12.kt

---
+++ src/main/kotlin/KotlinProgram12.kt
@@ -0,0 +1,19 @@
+fun main(){
+    var myString: String? = "Hello Kotlin"
+
+//    print(myString)
+
+//    // ?.let{}
+//    var count = myString?.Let{2+2}
+//    println(count)
+
+//    //elvis operator ?:
+//    val count2 = myString?.length ?: -5
+//    println(count2)
+
+
+    //!!
+    val count3 = myString!!.length
+    println(count3)
+
+}
