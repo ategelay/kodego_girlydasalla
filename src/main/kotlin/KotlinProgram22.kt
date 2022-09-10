0592e905c25684f50f6e4829a10c63fc6962e669
girly <girly.dasalla@gmail.com>
2022-09-10 13:53:58 +0800

    initial commit for KotlinProgram22.kt

---
+++ src/main/kotlin/KotlinProgram22.kt
@@ -0,0 +1,42 @@
+fun main() {
+//    displayName()
+    displayMessage("Hello!")
+//    println("Enter number 1:")
+//    var number: Int = readln().toInt()
+//
+//    println("The Cube is ${number * number * number}")
+//
+//    println("Enter number 2:")
+//    var number2: Int = readln().toInt()
+//
+//    println("The Cube is ${number2 * number2 * number2}")
+
+}
+////
+//////no return no parameter
+////fun displayName (){
+////    println("Peter")
+//}
+
+//no return with 1 paramenter
+fun displayMessage (msg:String){
+    println("The message is \"$msg\"")
+}
+
+//function in Kotlin -> java called it methods -> stored procedure -> sub procedure
+
+
+
+
+    //built-in/standard functions in kotlin
+    //they have open and close, starts with small letter // abstraction (no need to know whats behind the scene
+
+    //println()
+    //toInt()
+    //readln()
+    //main()
+    //arrayOf()
+    //listOf()
+
+// user-defined function
+
