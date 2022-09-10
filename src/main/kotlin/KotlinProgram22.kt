59886b34c1776d297d85429cc3d152a9c70eebf3
girly <girly.dasalla@gmail.com>
2022-09-10 14:10:43 +0800

    2nd commit for KotlinProgram22.kt

--- src/main/kotlin/KotlinProgram22.kt
+++ src/main/kotlin/KotlinProgram22.kt
@@ -1,32 +1,35 @@
 fun main() {
 //    displayName()
-    displayMessage("Hello!")
-//    println("Enter number 1:")
-//    var number: Int = readln().toInt()
-//
-//    println("The Cube is ${number * number * number}")
-//
-//    println("Enter number 2:")
-//    var number2: Int = readln().toInt()
-//
-//    println("The Cube is ${number2 * number2 * number2}")
+
+//    displayMessage("Hello!")
+
+    println("Enter your name:")
+    var name :String = readln()
+
+    println("Enter your age:")
+    var age :Int = readln().toInt()
+
+    displayNameAndAge(name,age)
 
 }
-////
-//////no return no parameter
-////fun displayName (){
-////    println("Peter")
-//}
+
+//no return no parameter
+fun displayName(){
+    println("Peter")
+}
 
 //no return with 1 paramenter
 fun displayMessage (msg:String){
     println("The message is \"$msg\"")
 }
 
-//function in Kotlin -> java called it methods -> stored procedure -> sub procedure
+//no return with 2 paramenter
+fun displayNameAndAge (name: String,age: Int){
+    println("Your name is $name")
+    println("Your name is $age")
+}
 
-
-
+    //function in Kotlin -> java called it methods -> stored procedure -> sub procedure
 
     //built-in/standard functions in kotlin
     //they have open and close, starts with small letter // abstraction (no need to know whats behind the scene
@@ -38,5 +41,5 @@ fun displayMessage (msg:String){
     //arrayOf()
     //listOf()
 
-// user-defined function
+    // user-defined function
 
