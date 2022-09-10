5ebc9addd398cf9148d2376ed1efcf50b692a798
girly <girly.dasalla@gmail.com>
2022-09-10 11:27:51 +0800

    initial commit for KotlinProgram21.kt

---
+++ src/main/kotlin/KotlinProgram21.kt
@@ -0,0 +1,23 @@
+fun main(){
+        //instantiation - process of creating an object
+        //any name of the object sample myObject
+    var myObject = MyClass("Hello World")
+
+    var myObject2 = MyClass("hi World")
+
+}
+class MyClass(value : String){
+    init{
+        println(value)
+    }
+}
+
+/*
+Class -> objects (class ginagamit sa pag create ng object)
+object -> variable
+       -> functions(process)
+uppercase -> first letter
+String
+Int
+
+ */
