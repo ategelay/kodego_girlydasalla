29befb6557bf5d77205e38cb5be3e2f75d09c570
girly <girly.dasalla@gmail.com>
2022-09-17 23:56:54 +0800

    2nd commit for Activity02-C.kt

--- src/main/kotlin/exercises/Activity02-C.kt
+++ src/main/kotlin/exercises/Activity02-C.kt
@@ -1,34 +1,9 @@
 package exercises
-//
-//fun main(){
-//    println("List of Items from Grocery Store")
-//    println("W for Wet Goods")
-//    println("D for Dry Goods")
-//
-//    //list of grocery wet
-//    var string1 : String = "W Tender Juicy Hotdog"
-//    var string2 : String = "W Magnolia Chicken Whole"
-//    var string3 : String = "W Magnolia Icecream Cookies and Cream"
-//
-//    //list of grocery dry
-//    var string4 : String = "D Gardenia Wheat Bread"
-//    var string5 : String = "D Huggies Diaper Pants XL"
-//    var string6 : String = "D Century Tuna Flakes in Oil"
-//
-//    //string character
-//    println(string1[0])
-//    println("$string1")
-//    println("$string2")
-//    println("$string3")
-//
-//    println(string4[0])
-//    println("$string4")
-//    println("$string5")
-////    println("$string6")
-//}
-//***********************************************************8888
 
 fun main() {
+
+    println("List of Items from Grocery Store")
+
     var myItems = setOf("hotdog, Whole Chicken")
     //mutable set
     var myItems2 = mutableSetOf<String>()
