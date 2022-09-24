61d5cc00b7a54ab2d333a33c44b00cf7122ce1ed
girly <girly.dasalla@gmail.com>
2022-09-24 11:42:03 +0800

    2nd commit for Main.kt

--- src/main/kotlin/com/oop/Main.kt
+++ src/main/kotlin/com/oop/Main.kt
@@ -1,7 +1,8 @@
 package com.oop
 
 fun main() {
-    var joeStudent = Student ()
+    var joeStudent = Student (123456)
     println(joeStudent.name)
-    joeStudent.age = -1
+    joeStudent.displayStudentId()
+    joeStudent.displayName()
 }
